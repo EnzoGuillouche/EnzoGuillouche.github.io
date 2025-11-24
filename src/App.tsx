d42565dbdfd27cd3e5b2542cb977a8de20eb43b2
EnzoGuillouche <145991192+EnzoGuillouche@users.noreply.github.com>
2025-11-24 22:27:07 +0100

    added languages hihi

--- src/App.tsx
+++ src/App.tsx
@@ -42,15 +42,15 @@ function App() {
 
         <h1>{t("tech_arsenal")}</h1>
 
-        <SkillContainer skillIds={[6, 7, 8, 9, 10, 11]}></SkillContainer>
+        <SkillContainer skillIds={[8, 9, 10, 11, 12, 13]}></SkillContainer>
 
         <h2>Web</h2>
 
-        <SkillContainer skillIds={[0, 1, 2, 3, 4, 5]}></SkillContainer>
+        <SkillContainer skillIds={[0, 1, 2, 3, 4, 5, 6, 7]}></SkillContainer>
 
         <h2>{t("tools")}</h2>
 
-        <SkillContainer skillIds={[12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22]}></SkillContainer>
+        <SkillContainer skillIds={[14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24]}></SkillContainer>
 
       </div>
 
